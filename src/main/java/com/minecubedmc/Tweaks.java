e79032b5bd6ed5b0428dd28b6ccb418d469d36fa
valentinotuksa <valentinotuksa@protonmail.com>
2022-05-20 21:31:18 +0200

    Added seasonal giant crop feature
    Signed-off-by: Valentinotuksa <valentinotuksa@protonmail.com>

--- src/main/java/com/minecubedmc/Tweaks.java
+++ src/main/java/com/minecubedmc/Tweaks.java
@@ -3,17 +3,22 @@ package com.minecubedmc;
 //import com.minecubedmc.items.Items;
 import com.minecubedmc.listeners.*;
 
+
+import me.casperge.realisticseasons.api.SeasonsAPI;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
 public final class Tweaks extends JavaPlugin {
 
+    private SeasonsAPI sapi;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        this.getLogger().info("Enabled");
+        sapi = SeasonsAPI.getInstance();
         this.registerListeners();
+        this.getLogger().info("Enabled");
     }
 
     @Override
@@ -28,12 +33,18 @@ public final class Tweaks extends JavaPlugin {
                         new PlayerInteractAtEntityListener(this),
                         new HangingBreakByEntityListener(this),
                         new IronGolemItemDrop(this),
-                        new SaplingGrowEvent(this)
+                        new SaplingGrowEvent(this),
+                        new CropGrowEvent(this)
+
 
                 ).
                 forEach(
                         listener -> this.getServer().getPluginManager()
                                 .registerEvents(listener, this)
                 );
+    }
+
+    public SeasonsAPI getSapi(){
+        return sapi;
     }
 }
--- src/main/java/com/minecubedmc/listeners/CropGrowEvent.java
+++ src/main/java/com/minecubedmc/listeners/CropGrowEvent.java
@@ -1,11 +1,15 @@
 package com.minecubedmc.listeners;
 
 import com.minecubedmc.Tweaks;
+import dev.lone.itemsadder.api.CustomBlock;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.Ageable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockGrowEvent;
+
+import java.util.Random;
 
 public class CropGrowEvent implements Listener {
 
@@ -14,13 +18,32 @@ public class CropGrowEvent implements Listener {
     public CropGrowEvent (Tweaks plugin){    this.plugin = plugin;  }
 
     @EventHandler
-    public void onSaplingGrow(BlockGrowEvent e){
-        Block block = e.getBlock();
-        if (block.getType().equals( Material.CARROT )){
-            e.setCancelled(true);
-            block.setType(Material.AIR);
+    public void onCropGrow(BlockGrowEvent e){
+        Block eventBlock = e.getBlock();
+        Ageable ageable = (Ageable) eventBlock.getBlockData();
+        String season = plugin.getSapi().getSeason(eventBlock.getWorld()).toString();
 
-            plugin.getLogger().info("Carrot sapling tried to grow!");
+        if (eventBlock.getType().equals( Material.CARROT )){
+            //Check if its optimal season
+            if (ageable.getAge() != 7){
+                return;
+            }
+
+            //Check if the crop is fully grown
+            if (!season.equals("SUMMER")){
+                return;
+            }
+
+            //Chance for crop to be giant
+            if (new Random().nextInt(100) > 50 ){
+                e.setCancelled(true);
+                CustomBlock giantCarrot = CustomBlock.getInstance("minecubed:giant_carrot");
+                giantCarrot.place(eventBlock.getLocation());
+                plugin.getLogger().info("Giant carrot grew!");
+            }
+
+            plugin.getLogger().info("Regular carrot grew!");
+
         }
 
     }
